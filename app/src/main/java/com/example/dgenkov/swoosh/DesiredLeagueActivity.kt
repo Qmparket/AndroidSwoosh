c73f290a3317b73732e66fa08f15afea8a867272
d.genkov <d.genkov@d.genkov>
2018-12-13 16:32:45 +0200

    third layout

--- app/src/main/java/com/example/dgenkov/swoosh/DesiredLeagueActivity.kt
+++ app/src/main/java/com/example/dgenkov/swoosh/DesiredLeagueActivity.kt
@@ -1,12 +1,22 @@
 package com.example.dgenkov.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_desired_league.*
 
 class DesiredLeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desired_league)
+
+        nextButton.setOnClickListener {
+            val skillIntent = Intent(this,DifficultyLevel::class.java)
+            startActivity(skillIntent)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/dgenkov/swoosh/DifficultyLevel.kt
@@ -0,0 +1,14 @@
+package com.example.dgenkov.swoosh
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DifficultyLevel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.difficulty_level)
+
+    }
+}
